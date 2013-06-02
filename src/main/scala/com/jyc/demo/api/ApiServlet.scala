eef67261bec934f3de29b2cffe81fca4d2ecb553
Nate Buwalda <natebuwalda@gmail.com>
2013-06-01 23:30:34 -0500

    Added post support

--- src/main/scala/com/jyc/demo/api/ApiServlet.scala
+++ src/main/scala/com/jyc/demo/api/ApiServlet.scala
@@ -4,20 +4,33 @@ import javax.servlet.http.{HttpServletResponse, HttpServletRequest, HttpServlet}
 import com.jyc.demo.domain.Employee
 import com.jyc.demo.logging.ConsoleLogger._
 import Json._
+import scala.beans.BeanProperty
+import org.apache.commons.io.IOUtils
 
 class ApiServlet extends HttpServlet {
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse) {
     val employee = Employee("Nate", "Buwalda", "Consultant", 34, 1000000.00)
 
-    val jsonResponse = performAndLog(toJson(employee))
+    writeResponse(response, performAndLog(toJson(employee)))
+  }
 
+  override def doPost(request: HttpServletRequest, response: HttpServletResponse) {
+    val newEmployeeJson = performAndLog(IOUtils.toString(request.getInputStream, request.getCharacterEncoding))
+    val newEmployee = fromJson(newEmployeeJson, classOf[Employee])
+
+    import newEmployee._
+    writeResponse(response, performAndLog(toJson(ResultMessage("Added %s %s".format(firstName, lastName)))))
+  }
+
+  private def writeResponse(response: HttpServletResponse, jsonResponse: String) {
     response.getWriter.println(jsonResponse)
     response.setContentType("application/json")
     response.setStatus(HttpServletResponse.SC_OK)
   }
-
 }
+
+case class ResultMessage(@BeanProperty message: String)
 
 
 
--- src/main/scala/com/jyc/demo/api/Json.scala
+++ src/main/scala/com/jyc/demo/api/Json.scala
@@ -1,9 +1,11 @@
 package com.jyc.demo.api
 
-import org.codehaus.jackson.map.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.databind.ObjectMapper
 
 object Json {
   val mapper = new ObjectMapper()
+  mapper.registerModule(DefaultScalaModule)
 
   def toJson[T](bean: T): String = mapper.writeValueAsString(bean)
   def fromJson[T](json: String, clazz: Class[T]): T = {
